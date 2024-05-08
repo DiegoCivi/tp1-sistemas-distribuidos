6793e831abbf215323fba27aacb2226b2cd7637d
bonshot <faguirrea@fi.uba.ar>
2024-05-08 10:55:49 -0300

    Generator almost working

--- generate_compose.py
+++ generate_compose.py
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Script to generate docker-compose-dev.yaml according to multiple config files named config_file_Qi 
 # where Qi es the query number i
 # Usage: ./generate_compose.sh
@@ -12,37 +10,35 @@
 # - service_name, dockerfile_path
 # + env_vars: key1=value1,key2=value2
 
-# Define el nombre del archivo de configuración
+# Define config file
 config_file = "config_file_queries.config"
 
-# Diccionario para almacenar las variables de entorno de cada servicio
+# Dictionary to store the environment variables of each service
 env_vars = {}
 
-# Lista para almacenar los nombres de los servicios y las rutas del Dockerfile
+# List for storing the services and their Dockerfile paths
 services = []
 
-# Leer el archivo de configuración línea por línea
+# Read the configuration file
 with open(config_file, "r") as file:
     service_name, dockerfile_path = None, None
     for line in file:
         line = line.strip()
         if line.startswith("-"):
-            # Procesar línea de nombre de servicio y Dockerfile
-            line = line[2:]  # Eliminar el prefijo '-'
+            # Service name and Dockerfile path
+            line = line[1:]  # Delete the prefix '-'
             service_name, dockerfile_path = line.split("$")
             services.append((service_name.strip(), dockerfile_path.strip()))
         elif line.startswith("+"):
-            # Procesar línea de variables de entorno
-            line = line[2:]  # Eliminar el prefijo '+'
+            # Environment variables
+            line = line[1:]  # Delete the prefix '+'
+            print(line)
             env_vars[service_name] = dict(item.split("=") for item in line.split("$"))
         else:
             continue
 
-# Ahora, env_vars contiene las variables de entorno de cada servicio
-# Y services contiene los nombres de los servicios y las rutas del Dockerfile
-
-# Lógica para generar docker-compose-dev2.yaml
-with open("docker-compose-dev2.yaml", "w") as outfile:
+# Generate docker-compose-dev2.yaml
+with open("docker-compose-dev.yaml", "w") as outfile:
     outfile.write("services:\n")
     # Escribir servicios predefinidos
     outfile.write("  rabbitmq:\n")
@@ -105,31 +101,29 @@ with open("docker-compose-dev2.yaml", "w") as outfile:
     outfile.write("      - EOF_REVIEWS_MAX_SUBS=6\n")
     outfile.write("\n")
     for service_name, dockerfile_path in services:
-        # Escribir información del servicio
-        outfile.write(f"  {service_name}:\n")
+        outfile.write(f"  {service_name}0:\n")
         outfile.write(f"    container_name: {service_name}\n")
         outfile.write(f"    build:\n")
         outfile.write(f"      context: .\n")
         outfile.write(f"      dockerfile: {dockerfile_path}\n")
         if service_name in env_vars:
-            # Escribir variables de entorno si están definidas para este servicio
+            # Write environment variables
             outfile.write("    environment:\n")
             for key, value in env_vars[service_name].items():
                 outfile.write(f"      - {key}={value}\n")
-            # Verificar si existe la variable WORKERS_QUANTITY
+            # Write as many workers as specified in WORKERS_QUANTITY
             if "WORKERS_QUANTITY" in env_vars[service_name]:
                 workers_quantity = int(env_vars[service_name]["WORKERS_QUANTITY"])
-                # Crear tantos contenedores como se especifique en WORKERS_QUANTITY
-                for i in range(workers_quantity):
-                    outfile.write(f"  {service_name}_worker_{i}:\n")
-                    outfile.write(f"    container_name: {service_name}_worker_{i}\n")
+                
+                for i in range(workers_quantity - 1):
+                    outfile.write(f"  {service_name}{i + 1}:\n")
+                    outfile.write(f"    container_name: {service_name}{i}\n")
                     outfile.write(f"    build:\n")
                     outfile.write(f"      context: .\n")
                     outfile.write(f"      dockerfile: {dockerfile_path}\n")
-                    # Copiar las variables de entorno al nuevo contenedor
+                    outfile.write(f"    environment:\n")
                     for key, value in env_vars[service_name].items():
                         if key == "WORKER_ID":
-                            outfile.write(f"    environment:\n")
                             outfile.write(f"      - {key}={i}\n")
                         else:
                             outfile.write(f"      - {key}={value}\n")
--- workers/top_10_worker.py
+++ workers/top_10_worker.py
@@ -7,7 +7,7 @@ def main():
     top_n = int(os.getenv('TOP_N'))
     data_source_name = os.getenv('DATA_SOURCE_NAME')
     data_output_name = os.getenv('DATA_OUTPUT_NAME')
-    workers_quantity = int(os.getenv('WORKERS_QUANTITY'))
+    workers_quantity = int(os.getenv('WORKER_QUANTITY'))
     last = True if os.getenv('LAST') == '1' else False
     
     worker = TopNWorker(data_source_name, data_output_name, workers_quantity, top_n, last)
