11028b20702cd443ba74370efe3230b26f47c7b7
DiegoCivi <dcivini@fi.uba.ar>
2024-06-11 19:42:07 -0300

    Fix Logger class. Workers now persist data on disk correctly

--- lib/logger.py
+++ lib/logger.py
@@ -1,28 +1,29 @@
 import pickle
 import os
 
+VOLUME_PATH = './persisted_data/'
+
 class Logger:
 
-    def __init__(self, file):
-        self.file = file
+    def __init__(self, file_name):
+        self.file_path = VOLUME_PATH + file_name
 
     def persist(self, content):
-        f = open(self.file, 'ab')
+        f = open(self.file_path, 'ab')
         pickle.dump(content, f)
         f.write(b'\n')
         f.flush()
         f.close()
     
     def read_persisted_data(self):
-        f = open(self.file, 'ab')
-        with open('./log.txt', 'rb') as f:
-            try:  # catch OSError in case of a one line file 
-                f.seek(-2, os.SEEK_END)
-                while f.read(1) != b'\n':
-                    f.seek(-2, os.SEEK_CUR)
-            except OSError:
-                f.seek(0)
-            last_line = pickle.load(f)
+        f = open(self.file_path, 'ab')
+        try:  # catch OSError in case of a one line file 
+            f.seek(-2, os.SEEK_END)
+            while f.read(1) != b'\n':
+                f.seek(-2, os.SEEK_CUR)
+        except OSError:
+            f.seek(0)
+        last_line = pickle.load(f)
         
         f.close()
 
